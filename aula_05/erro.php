adbea6ee9c14547b2de1f6ff8a3af1cfa4aa0845
Paola Onório Zaratini <159515333+paolaonzara@users.noreply.github.com>
2025-03-20 17:29:35 -0300

    Add files via upload

---
+++ aula_05/erro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Erro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo_erro.css">
+</head>
+<body>
+    <div class="container">
+        <div class="custom-bg text-dark">
+            <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold">404</h1>
+                    <p class="fs-2 fw-medium mt-4">Oops! Usuario Não Encontrado!</p>
+                    <p class="mt-4 mb-5">Clique no botão abaixo e tente fazer login novamente.</p>
+                    <a href="index.php" class="btn btn-light fw-semibold rounded-pill px-4 py-2 custom-btn">
+                        Ir para o início
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
