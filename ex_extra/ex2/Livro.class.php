3b5b699f94da300666ba8ec089e6d15c660c167f
Paola Onório Zaratini <159515333+paolaonzara@users.noreply.github.com>
2025-06-12 08:21:29 -0300

    Add files via upload

---
+++ ex_extra/ex2/Livro.class.php
@@ -0,0 +1,40 @@
+<?php
+    class Livro{
+        private $Titulo;
+        private $Autor;
+        private bool $Disponivel;
+
+        public function __construct($tit,$autor,$disp){
+            $this->Titulo = $tit;
+            $this->Autor = $autor;
+            $this->Disponivel=$disp;
+        }
+        public function getTitulo(){
+            return $this->Titulo;
+        }
+        public function getAutor(){
+            return $this->Autor;
+        }
+        public function getDisponibilidade(){
+            return $this->Disponivel;
+        }
+        public function emprestar(){
+            if ($this->Disponivel) {
+                $this->Disponivel = false;  
+                echo "O livro '{$this->Titulo}' foi emprestado.";
+            } else {
+                echo "O livro$this->Titulo}' disponível.";
+            }
+        }
+    
+        }
+
+
+
+
+
+
+
+
+
+?>
