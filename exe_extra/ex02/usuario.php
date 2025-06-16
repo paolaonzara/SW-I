df23145d986a7fa635efaec7162722f3ce1b4fda
Paola Onório Zaratini <159515333+paolaonzara@users.noreply.github.com>
2025-06-16 08:37:45 -0300

    Update usuario.php

--- exe_extra/ex02/usuario.php
+++ exe_extra/ex02/usuario.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'Livro.class.php';
 
-$usr1= new Livro("Pequeno Príncipe","Fulano",false);
+$usr1= new Livro("Lua de Larvas","Sally Gardner",false);
 
 echo $usr1->exibirStatus();
 
